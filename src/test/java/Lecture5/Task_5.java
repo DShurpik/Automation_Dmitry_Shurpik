af9e83e4fedf1f83967a57258677e417926b28ea
= <=>
2022-08-25 13:50:19 +0300

    add Task_5

--- src/test/java/Lecture5/Task_5.java
+++ src/test/java/Lecture5/Task_5.java
@@ -5,6 +5,8 @@ import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.Select;
 import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
 
@@ -19,6 +21,14 @@ public class Task_5 {
     public void precondition(){
         SimpleDriver simpleDriver = new SimpleDriver();
         getWebDriver().get("https://masterskayapola.ru/kalkulyator/laminata.html");
+    }
+    @AfterTest
+    public void postconditions(){
+        getWebDriver().quit();
+    }
+    @BeforeMethod
+    public void reload() {
+        getWebDriver().navigate().refresh();
     }
     @Test
     public void test1(){
@@ -47,6 +57,61 @@ public class Task_5 {
         );
         Assert.assertEquals(expectedResult, getActualResult());
     }
+    @Test
+    public void test2(){
+        WebElement setRoomWidth = findElement("calc_roomwidth");
+        setRoomWidth.sendKeys(Keys.BACK_SPACE, "12");
+        WebElement setRoomHeight = findElement("calc_roomheight");
+        setRoomHeight.sendKeys(Keys.BACK_SPACE, "9");
+        WebElement setLaminateWidth = findElement("calc_lamwidth");
+        setLaminateWidth.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "1800");
+        WebElement setLaminateHeigh = findElement("calc_lamheight");
+        setLaminateHeigh.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "300");
+        WebElement setIpack = findElement("calc_inpack");
+        setIpack.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, "9");
+        WebElement setPrice = findElement("calc_price");
+        setPrice.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "1000");
+        Select selectDirec = new Select(getWebDriver().findElement(By.name("calc_direct")));
+        selectDirec.selectByValue("tow");
+        WebElement setBias = findElement("calc_bias");
+        setBias.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "500");
+        WebElement setWallDist = findElement("calc_walldist");
+        setWallDist.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, "200");
+        WebElement submit = getWebDriver().findElement(By.cssSelector("[value='Рассчитать']"));
+        submit.click();
+        List<String> expectedResult = Arrays.asList(
+                "103.84 м2.", "203 шт.", "23 шт.", "111780 руб.", "4 шт.", "10 шт."
+        );
+        Assert.assertEquals(expectedResult, getActualResult());
+    }
+    @Test
+    public void test3(){
+        WebElement setRoomWidth = findElement("calc_roomwidth");
+        setRoomWidth.sendKeys(Keys.BACK_SPACE, "14.5");
+        WebElement setRoomHeight = findElement("calc_roomheight");
+        setRoomHeight.sendKeys(Keys.BACK_SPACE, "9.5");
+        WebElement setLaminateWidth = findElement("calc_lamwidth");
+        setLaminateWidth.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "2500");
+        WebElement setLaminateHeigh = findElement("calc_lamheight");
+        setLaminateHeigh.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "400");
+        WebElement setIpack = findElement("calc_inpack");
+        setIpack.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, "5");
+        WebElement setPrice = findElement("calc_price");
+        setPrice.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "2000");
+        Select selectDirec = new Select(getWebDriver().findElement(By.name("calc_direct")));
+        selectDirec.selectByValue("tow");
+        WebElement setBias = findElement("calc_bias");
+        setBias.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, Keys.BACK_SPACE, "500");
+        WebElement setWallDist = findElement("calc_walldist");
+        setWallDist.sendKeys(Keys.BACK_SPACE, Keys.BACK_SPACE, "400");
+        WebElement submit = getWebDriver().findElement(By.cssSelector("[value='Рассчитать']"));
+        submit.click();
+        List<String> expectedResult = Arrays.asList(
+                "132.99 м2.", "140 шт.", "28 шт.", "280000 руб.", "0 шт.", "7 шт."
+        );
+        Assert.assertEquals(expectedResult, getActualResult());
+    }
+
 
     private WebElement findElement(String name){
         getWebDriver().findElement(By.name(name));
